5cf4d779ac684602e3476577be5a956ae6694bde
omedcodes <omedsafi21@gmail.com>
2026-02-22 12:54:33 +0100

    Feat add HTML api functions and override browsers default behaviour

--- kanban-board/index.js
+++ kanban-board/index.js
@@ -12,3 +12,17 @@ for (const list of lists) {
   list.addEventListener("dragleave", dragLeave);
   list.addEventListener("drop", dragDrop);
 }
+
+function dragStart(e) {
+  // Alloews to find out the drop location for the element where to be moved at
+  e.dataTransfer.setData("text/plain", this.id);
+}
+
+function dragEnd() {
+  console.log("Drag ended");
+}
+
+function dragOver(e) {
+  // Override the browsers default behaviour so dropping on other elements works
+  e.preventDefault();
+}
